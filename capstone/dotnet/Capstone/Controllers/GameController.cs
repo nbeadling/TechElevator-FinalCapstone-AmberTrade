518ed11d84e923f5a6ab557b41bda0f9dda3c5aa
Nicholas Beadling <nbeadling@hotmail.com>
2022-04-12 16:55:41 -0400

    GameController

---
+++ capstone/dotnet/Capstone/Controllers/GameController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Authorization; 
+
+namespace Capstone.Controllers
+{
+    [Route("game")]
+    [ApiController]
+    [Authorize]
+    public class GameController : Controller
+    {
+        private IGameDAO gameDAO;
+
+        public GameController(IGameDAO gameDAO)
+        {
+            this.gameDAO = gameDAO;
+        }
+
+        [HttpPost()]
+
+        public ActionResult<Game> CreateGame(Game game)
+        {
+            Game added = gameDAO.CreateGame(game);
+            return Created($"/game/{added.GameId}", added); 
+        }
+
+        [HttpGet("{id}")]
+        public Game GetGameByGameId(int id)
+        {
+            return gameDAO.GetGamesById(id); 
+        }
+
+        //[HttpGet("/users/game")]
+
+        //public Game GetGameByUserId(int id)
+        //{
+        //    return gameDAO.GetGamesByUser(id); 
+        //}
+    }
+}
