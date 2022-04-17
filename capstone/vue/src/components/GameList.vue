e2b212a7565cc78652e67781a0b5ccf4209ad530
Andrew Chambers <andrew.m.chambers@gmail.com>
2022-04-16 23:15:54 -0400

    API methods and mutations made for: GET userGames, createGame,and buyStocks

--- capstone/vue/src/components/GameList.vue
+++ capstone/vue/src/components/GameList.vue
@@ -6,7 +6,7 @@
               <li>List of games that the user is a member of</li>
               <li>Details including: Game name, start date, end date</li>
               <li> Links to view details of each game individually </li>
-              <li> be able to sort users in a game by portfolio value, alphabetical </li>
+              <li> be able to sort users in a game by portfolio value, alphabetical? </li>
             </ul>
             <game-detail></game-detail>
 
@@ -16,7 +16,7 @@
 
 <script>
 import GameDetail from './GameDetail.vue';
-
+import ApiService from '../services/ApiService.js';
 
 
 export default {
@@ -30,11 +30,33 @@ export default {
         endDate: '',
         gameId: '', 
         gameName: '', 
-
       },
       isLoading: true
     };
   },
+  created(){
+    this.retrieveGames(this.profile.userId);
+  },
+
+  computed: {
+    profile(){
+      return this.$store.state.user;
+    },
+  },
+
+  methods:{
+    retrieveGames(userId) {
+      ApiService
+      .getUserGames(userId)
+      .then(response => {
+        this.$store.commit("SET_GAMES_LIST", response.data);
+
+        if (this.$route.name == "Home" && response.status === 200 && response.data.length > 0) {
+          this.$router.push(`/board/${response.data[0].id}`);
+        }
+      });
+    },
+  }
   
   
 };
